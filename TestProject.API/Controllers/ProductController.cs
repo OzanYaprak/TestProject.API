76a943ebd3a5d1fd3ebd34f9839bea17cff08e1d
Ozan YAPRAK <ozanerhanyaprak@icloud.com>
2023-09-01 21:29:03 +0300

    Update 1.09

--- TestProject.API/Controllers/ProductController.cs
+++ TestProject.API/Controllers/ProductController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TestProject.API.Data;
 using TestProject.API.Models;
 
 namespace TestProject.API.Controllers
@@ -7,22 +9,26 @@ namespace TestProject.API.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private static List<Product> _products = new List<Product>
-        {   new Product { ProductID = 1, ProductName = "Marker Kalem",  ProductPrice = "300" },
-            new Product { ProductID = 2, ProductName = "Dolma Kalem",   ProductPrice ="2000"},
-            new Product { ProductID = 3, ProductName = null, ProductPrice =  null},
-        };
+
+        private readonly TestProjectDBContext _dbContext;
+
+        public ProductController(TestProjectDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
 
         [HttpGet]
         public async Task<ActionResult<List<Product>>> Get()
         {
-            return Ok(_products);
+            var productList = await _dbContext.Products.ToListAsync();
+
+            return Ok(productList);
         }
 
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> Get(int id)
         {
-            var product = _products.Find(x => x.ProductID == id);
+            var product = await _dbContext.Products.FirstOrDefaultAsync(x => x.ProductID == id);
             if (product == null) { return BadRequest("Ürün ID Bulunamadı"); }
             return Ok(product);
         }
@@ -30,32 +36,43 @@ namespace TestProject.API.Controllers
         [HttpPost]
         public async Task<ActionResult<List<Product>>> AddProduct(Product product)
         {
-            _products.Add(product);
-            return Ok(_products);
+            _dbContext.Products.Add(product);
+            await _dbContext.SaveChangesAsync();
 
-            //Veya aşağıdaki gibi id eşit olanı getirebilir.
-            //return Ok(_products.Where(x=>x.ProductID == product.ProductID));
+            var productList = await _dbContext.Products.ToListAsync();
+
+            return Ok(productList);
         }
 
         [HttpPut]
         public async Task<ActionResult<List<Product>>> UpdateProduct(Product product)
         {
-            var updatedProduct = _products.Find(x => x.ProductID == product.ProductID);
+
+            var updatedProduct = await _dbContext.Products.FirstOrDefaultAsync(x => x.ProductID == product.ProductID);
             if (updatedProduct == null) { return BadRequest("Ürün ID Bulunamadı"); }
             updatedProduct.ProductName = product.ProductName;
             updatedProduct.ProductPrice = product.ProductPrice;
 
-            return Ok(_products);
+            await _dbContext.SaveChangesAsync();
+
+            var productList = await _dbContext.Products.ToListAsync();
+
+            return Ok(productList);
         }
 
-        [HttpDelete]
-        public async Task<ActionResult<List<Product>>> DeleteProduct (int id)
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<Product>>> DeleteProduct(int id)
         {
-            var deletedProduct = _products.Find(x=>x.ProductID == id);
+
+            var deletedProduct = await _dbContext.Products.FirstOrDefaultAsync(x => x.ProductID == id);
             if (deletedProduct == null) { return NotFound("Silinecek Ürün Bulunamadı"); }
 
-            _products.Remove(deletedProduct);
-            return Ok(_products);
+            _dbContext.Products.Remove(deletedProduct);
+            await _dbContext.SaveChangesAsync();
+
+            var productList = await _dbContext.Products.ToListAsync();
+
+            return Ok(productList);
         }
     }
 }
